2bb37cd5a8183c6d3635ca6b3c8379f12abb0fc8
parfaitandre5@gmail.com <parfaitandre5@gmail.com>
2024-06-27 04:17:53 +0100

    first commit

---
+++ src/main/java/student_spring_app/student/StudentApplication.java
@@ -0,0 +1,13 @@
+package student_spring_app.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+}
